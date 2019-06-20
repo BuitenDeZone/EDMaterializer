4ccd631a2a8451e7f202ae620f95fb95830318a7
Jan Vansteenkiste <jan@vstone.eu>
2019-06-20 17:16:00 +0200

    Adjusted the test classes for the new plugin prefs

--- test_material_frame.py
+++ test_material_frame.py
@@ -9,7 +9,9 @@ import theme
 
 # Materializer plugin components
 from material_ui import MaterialAlertListFrame
-from material_api import MaterialMatch, Materials
+from material_ui import MaterialAlertListSettings
+from material_api import MaterialMatch, Materials, Rarities
+from load import update_alert_frame
 
 
 class Application(tk.Frame):
@@ -53,10 +55,91 @@ def add_matches_4():
     """Add test matches."""
     APP.materialList.add_matches("2", MATCHES)
 
+
+TEST_SYSTEM = "Irk"
+TEST_MATERIAL_VERY_COMMON_1 = Materials.CARBON
+TEST_MATERIAL_VERY_COMMON_2 = Materials.SULPHUR
+TEST_MATERIAL_COMMON_1 = Materials.VANADIUM
+TEST_MATERIAL_COMMON_2 = Materials.ARSENIC
+TEST_MATERIAL_RARE = Materials.CADMIUM
+TEST_MATERIAL_VERY_RARE = Materials.POLONIUM
+
+RULES = {
+    "C>=10.00", "As>=0.00"
+}
+
+TEST_SETS = {
+    'Irk 1': [
+        {
+            "Name": TEST_MATERIAL_VERY_COMMON_1.name.lower(),
+            "Percent": 19.242706
+        },
+        {
+            "Name": TEST_MATERIAL_VERY_COMMON_2.name.lower(),
+            "Percent": 17.019758
+        },
+        {
+            "Name": TEST_MATERIAL_COMMON_2.name.lower(),
+            "Percent": 7.028986
+        },
+        {
+            "Name": TEST_MATERIAL_VERY_RARE.name.lower(),
+            "Percent": 1.47208
+        }
+    ],
+    'Irk 2 B': [
+        {
+            "Name": TEST_MATERIAL_VERY_COMMON_2.name.lower(),
+            "Percent": 17.019758
+        },
+        {
+            "Name": TEST_MATERIAL_COMMON_1.name.lower(),
+            "Percent": 7.028986
+        },
+        {
+            "Name": TEST_MATERIAL_RARE.name.lower(),
+            "Percent": 1.47208
+        }
+    ],
+    'Irk 8 C 3': [
+        {
+            "Name": TEST_MATERIAL_VERY_COMMON_1.name.lower(),
+            "Percent": 7.028986
+        },
+        {
+            "Name": TEST_MATERIAL_VERY_COMMON_2.name.lower(),
+            "Percent": 7.028986
+        },
+        {
+            "Name": TEST_MATERIAL_COMMON_1.name.lower(),
+            "Percent": 7.028986
+        },
+        {
+            "Name": TEST_MATERIAL_COMMON_2.name.lower(),
+            "Percent": 7.028986
+        },
+        {
+            "Name": TEST_MATERIAL_RARE.name.lower(),
+            "Percent": 7.028986
+        },
+        {
+            "Name": TEST_MATERIAL_VERY_RARE.name.lower(),
+            "Percent": 7.028986
+        }
+    ]
+}
+
+TEST_FILTERS = MaterialAlertListSettings.translate_from_settings(RULES)
+
+def update_materials():
+    for planet, materials in TEST_SETS.items():
+        update_alert_frame(APP.materialList, TEST_SYSTEM, planet, materials, TEST_FILTERS)
+
+
 ROOT = tk.Tk()
 APP = Application(master=ROOT)
-APP.after(1000, add_matches_1)
-APP.after(2000, add_matches_2)
-APP.after(3000, add_matches_3)
-APP.after(4000, add_matches_4)
+APP.after(1000, update_materials)
+#APP.after(2000, add_matches_2)
+#APP.after(3000, add_matches_3)
+#APP.after(4000, add_matches_4)
 APP.mainloop()
--- test_prefsv2_frame.py
+++ test_prefsv2_frame.py
@@ -2,30 +2,73 @@
 This is a small app that does nothing to test the preferences frame.
 """
 
+import sys
 import Tkinter as tk
-from material_ui import MaterialAlertsListPreferencesFrame, MaterialAlertListSettings
+from material_ui import MaterialAlertListSettings
+from load import DEFAULT_THRESHOLDS, create_plugin_prefs
+from pprint import pprint
 
+this = sys.modules[__name__]  # For holding module globals
 
 class Application(tk.Frame):
     """
     Test application.
     """
 
-    def __init__(self, testSettings, master=None):
+    def __init__(self, filters, master=None):
         tk.Frame.__init__(self, master, width=400, height=300)
         self.master = master
-        self.testSettings = testSettings
-        self.grid()
+        self.filters = filters
         self.create_widgets()
+        self.grid()
+        master.protocol("WM_DELETE_WINDOW", self.exit)
 
     def create_widgets(self):
-        """Create widgets."""
+        self.prefsFrame = create_plugin_prefs(self, DEFAULT_THRESHOLDS, self.filters)
+        self.prefsFrame.grid(column=0, row=0, sticky=tk.N+tk.S+tk.W)
 
-        MaterialAlertsListPreferencesFrame(self, self.testSettings)
+
+    def exit(self):
+        try:
+            print("Bye!")
+            pprint(MaterialAlertListSettings.translate_to_settings(self.prefsFrame.get_material_filters()))
+        finally:
+            self.master.destroy()
 
 
-TESTLIST = ["As>=2.0", "Po>=2.5"]
+TESTLIST = ['Sb>=-100.00',
+             'As>=2.00',
+             'Cd>=-100.00',
+             'C>=-100.00',
+             'Cr>=16.40',
+             'Ge>=5.60',
+             'Fe>=-100.00',
+             'Mn>=-100.00',
+             'Hg>=-100.00',
+             'Mo>=-103.00',
+             'Ni>=-100.00',
+             'Nb>=-100.00',
+             'P>=-100.00',
+             'Po>=2.50',
+             'Ru>=-100.00',
+             'Se>=-100.00',
+             'S>=-100.00',
+             'Tc>=-100.00',
+             'Te>=-100.00',
+             'Sn>=-100.00',
+             'W>=-100.00',
+             'V>=-100.00',
+             'Y>=-100.00',
+             'Zn>=-100.00',
+             'Zr>=-100.00',
+             'Re>=-100.00',
+             'Pb>=-100.00',
+             'B>=-100.00']
+
 TESTSETTINGS = MaterialAlertListSettings.translate_from_settings(TESTLIST)
+# print "From test settings:"
+# for filter in TESTSETTINGS:
+#     pprint(filter.__str__())
 
 ROOT = tk.Tk()
 APP = Application(TESTSETTINGS, master=ROOT)
